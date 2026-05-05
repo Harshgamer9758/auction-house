1d32e2ad2e370d28fc29c1804ca9d53b8547c696
Harshgamer9758 <jadhaokishor08@gmail.com>
2026-05-05 15:29:08 +0530

    feat: add src/main/java/com/blockmart/auctionhouse/AuctionHouse.java

--- src/main/java/com/blockmart/auctionhouse/AuctionHouse.java
+++ src/main/java/com/blockmart/auctionhouse/AuctionHouse.java
@@ -1,43 +1,32 @@
 package com.blockmart.auctionhouse;
 
-import com.blockmart.auctionhouse.commands.AuctionHouseCommand;
+import com.blockmart.auctionhouse.commands.AuctionCommand;
 import com.blockmart.auctionhouse.database.DatabaseManager;
-import com.blockmart.auctionhouse.listeners.InventoryListener;
+import com.blockmart.auctionhouse.listeners.AuctionListener;
 import com.blockmart.auctionhouse.managers.AuctionManager;
-import com.blockmart.auctionhouse.managers.EscrowManager;
-import net.milkbowl.vault.economy.Economy;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.logging.Level;
 
 public final class AuctionHouse extends JavaPlugin {
 
     private static AuctionHouse instance;
     private DatabaseManager databaseManager;
     private AuctionManager auctionManager;
-    private EscrowManager escrowManager;
-    private Economy economy;
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
 
-        if (!setupEconomy()) {
-            getLogger().log(Level.SEVERE, "Vault not found or economy provider not registered! Disabling plugin.");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
         this.databaseManager = new DatabaseManager(this);
-        this.escrowManager = new EscrowManager(this, databaseManager, economy);
-        this.auctionManager = new AuctionManager(this, databaseManager, escrowManager, economy);
+        this.databaseManager.initialize();
 
-        getCommand("ah").setExecutor(new AuctionHouseCommand(this, auctionManager));
-        getServer().getPluginManager().registerEvents(new InventoryListener(auctionManager), this);
+        this.auctionManager = new AuctionManager(this, databaseManager);
+        this.auctionManager.loadActiveAuctions();
 
-        getLogger().log(Level.INFO, "AuctionHouse has been enabled!");
+        getCommand("ah").setExecutor(new AuctionCommand(this));
+        getServer().getPluginManager().registerEvents(new AuctionListener(this), this);
+
+        getLogger().info("AuctionHouse has been enabled!");
     }
 
     @Override
@@ -45,19 +34,7 @@ public final class AuctionHouse extends JavaPlugin {
         if (databaseManager != null) {
             databaseManager.shutdown();
         }
-        getLogger().log(Level.INFO, "AuctionHouse has been disabled!");
-    }
-
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        economy = rsp.getProvider();
-        return economy != null;
+        getLogger().info("AuctionHouse has been disabled!");
     }
 
     public static AuctionHouse getInstance() {
@@ -70,13 +47,5 @@ public final class AuctionHouse extends JavaPlugin {
 
     public AuctionManager getAuctionManager() {
         return auctionManager;
-    }
-
-    public EscrowManager getEscrowManager() {
-        return escrowManager;
-    }
-
-    public Economy getEconomy() {
-        return economy;
     }
 }
