f99cbbc98e5088b97eb27a17b985f2bf5bf2cb5f
Harshgamer9758 <jadhaokishor08@gmail.com>
2026-05-06 16:35:27 +0530

    feat: add src/main/java/com/blockmart/auctionhouse/AuctionHouse.java

--- src/main/java/com/blockmart/auctionhouse/AuctionHouse.java
+++ src/main/java/com/blockmart/auctionhouse/AuctionHouse.java
@@ -1,40 +1,69 @@
 package com.blockmart.auctionhouse;
 
 import com.blockmart.auctionhouse.commands.AuctionCommand;
-import com.blockmart.auctionhouse.database.DatabaseManager;
 import com.blockmart.auctionhouse.listeners.AuctionListener;
 import com.blockmart.auctionhouse.managers.AuctionManager;
+import com.blockmart.auctionhouse.managers.DatabaseManager;
+import com.blockmart.auctionhouse.utils.NBTUtils;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 public final class AuctionHouse extends JavaPlugin {
 
     private static AuctionHouse instance;
     private DatabaseManager databaseManager;
     private AuctionManager auctionManager;
+    private Economy economy;
+    private NBTUtils nbtUtils;
 
     @Override
     public void onEnable() {
         instance = this;
-        saveDefaultConfig();
+        Logger log = getLogger();
 
-        this.databaseManager = new DatabaseManager(this);
-        this.databaseManager.initialize();
+        if (!setupEconomy()) {
+            log.severe("Disabled due to no Vault dependency found or economy provider!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
-        this.auctionManager = new AuctionManager(this, databaseManager);
-        this.auctionManager.loadActiveAuctions();
+        this.nbtUtils = new NBTUtils();
 
-        getCommand("ah").setExecutor(new AuctionCommand(this));
-        getServer().getPluginManager().registerEvents(new AuctionListener(this), this);
+        File dataFolder = new File(getDataFolder(), "auction_house.db");
+        this.databaseManager = new DatabaseManager(this, dataFolder.getAbsolutePath());
+        this.databaseManager.loadDatabase();
 
-        getLogger().info("AuctionHouse has been enabled!");
+        this.auctionManager = new AuctionManager(this, databaseManager, economy);
+        this.auctionManager.loadActiveAuctions().join(); // Wait for auctions to load before plugin is fully enabled
+
+        getCommand("auction").setExecutor(new AuctionCommand(this, auctionManager, nbtUtils));
+        getServer().getPluginManager().registerEvents(new AuctionListener(auctionManager), this);
+
+        log.info("AuctionHouse has been enabled!");
     }
 
     @Override
     public void onDisable() {
         if (databaseManager != null) {
-            databaseManager.shutdown();
+            databaseManager.closeConnection();
         }
         getLogger().info("AuctionHouse has been disabled!");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
     }
 
     public static AuctionHouse getInstance() {
@@ -47,5 +76,9 @@ public final class AuctionHouse extends JavaPlugin {
 
     public AuctionManager getAuctionManager() {
         return auctionManager;
+    }
+
+    public NBTUtils getNbtUtils() {
+        return nbtUtils;
     }
 }
